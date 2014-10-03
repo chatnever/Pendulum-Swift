38de214d9f12b4a6d6c97b25fc2f78fcb95377ec
Dzung <Nguyen>
2014-10-03 14:48:26 +0700

    first commit

--- PendulumInSwift/ViewController.swift
+++ PendulumInSwift/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pendulum: UIImageView?
+    @IBOutlet weak var sliderAngle: UISlider?
+    
+    var _time: NSTimer?
+    var _angle, _angleDelta : Double!
+    let MAX_ANGLE = M_PI_4 / 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var size: CGSize = self.pendulum!.bounds.size
+        self.pendulum?.layer.anchorPoint = CGPointMake(0.5, 0)
+        self.pendulum?.frame = CGRectMake((self.view.bounds.size.width - size.width) / 2, 20, size.width, size.height)
+        
+        _angle = 0.0
+        _angleDelta = 0.05
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func startAnimation(sender: UISwitch) {
+        if sender.on {
+            _time = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "animatePendulum:", userInfo: nil, repeats: true)
+            _time?.fire()
+        } else {
+            _time?.invalidate()
+        }
+    }
+    
+    func animatePendulum(sender: NSTimer) {
+        _angle = _angle + _angleDelta
+        if ((_angle > MAX_ANGLE) | (_angle < -MAX_ANGLE)) {
+            _angleDelta = -_angleDelta
+        }
+        self.pendulum?.transform = CGAffineTransformMakeRotation(CGFloat(_angle))
+        NSLog("%3.2f - %3.2f", self.pendulum!.center.x, self.pendulum!.center.y)
+        pendulum?.isAnimating()
+    }
+    
+    @IBAction func changeAngle(sender: UISlider) {
+        self.pendulum?.transform = CGAffineTransformMakeRotation(CGFloat(MAX_ANGLE) * CGFloat(sender.value))
+        self.sliderAngle?.addSubview(pendulum!)
+        println("\(pendulum!)")
+    }
+    
 
 }
 
